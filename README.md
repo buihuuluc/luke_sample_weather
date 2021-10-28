94ca7ab444ceb0d20c5b4293b603309dbfc01301
buihuuluc <buihuuluc@gmail.com>
2021-10-28 11:10:43 +0700

    firt commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"#luke_sample_weather" 
---
+++ android/app/src/main/java/com/example/luke_sample_weather/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.luke_sample_weather;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
